509d90dc1eb6ea2d879bc05f68d5a86ba9a7b4fd
Praew <teeranan39@hotmail.com>
2017-08-05 14:48:08 +0700

    Praew

--- app/src/main/java/com/prabsattru/teeranan/myrbac/MainActivity.java
+++ app/src/main/java/com/prabsattru/teeranan/myrbac/MainActivity.java
@@ -3,11 +3,23 @@ package com.prabsattru.teeranan.myrbac;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.prabsattru.teeranan.myrbac.fragment.MainFragment;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+        // Add Fragment
+        if (savedInstanceState == null) {
+            MainFragment mainFragment = new MainFragment();
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .add(R.id.mainContainer, mainFragment)
+                    .commit();
+        }
+
+    } // Main Method
+} // Main Class
